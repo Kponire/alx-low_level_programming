bdd874377b614555a621522acad81d8e70518b2c
Oluwaponire <kponire@gmail.com>
2023-09-07 03:02:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main -  prints Programming is like building a multilingual puzzle
+ * Return: zero
+ */
+
 int main(void)
 {
 
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }

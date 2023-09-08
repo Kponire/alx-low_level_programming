1a1aa057de68210f115e9ebbff8641b3ab90cd55
Oluwaponire <kponire@gmail.com>
2023-09-08 10:09:30 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: prints all possible different combinations of two digits
+ * Return: 0 indicates success
+ */
+
+int main(void)
+{
+	int first_digit;
+	int second_digit;
+
+	for (first_digit = 0; first_digit <= 9; first_digit++)
+	{
+		for (second_digit = 9; second_digit > first_digit; second_digit--)
+		{
+			if(second_digit)
+			{
+				putchar(48 + first_digit);
+				putchar(48 + second_digit);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}

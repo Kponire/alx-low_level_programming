89c85d80ad3f5f2293fc0ec7e51a545f9e30a9e1
Oluwaponire <kponire@gmail.com>
2023-09-12 12:29:57 -0700

    ./8-24 | tail

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Entry point
+ * Description: prints every minute of the day of Jack Bauer
+ */
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 24; a++)
+	{
+		for (b = 0; b < 60; b++)
+		{
+			_putchar('0' + a / 10);
+			_putchar('0' + a % 10);
+			_putchar(':');
+			_putchar('0' + b / 10);
+			_putchar('0' + b % 10);
+			_putchar('\n');
+		}
+	}
+}
---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 indicates success
+ */
+
+int main(void)
+{
+	jack_bauer();
+	return (0);
+}

919b2dfa95807f7dacca221eee8659bb42a20a74
Oluwaponire <kponire@gmail.com>
2023-09-12 13:05:16 -0700

    ./10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @x: first parameter
+ * @y: second parameter
+ * Return: addition of x and y
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}

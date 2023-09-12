cefcfe3df2914c183d8434854030d4fd26bd08ea
Oluwaponire <kponire@gmail.com>
2023-09-12 09:36:49 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ * Description: prints the sign of a number
+ * @n: number to be checked
+ * Return: 1 if if n is greater than zero, -1 less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}

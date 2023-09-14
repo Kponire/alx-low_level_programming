1055525a9e5a189354fa6ac0e5cf02b895d6b7f9
Oluwaponire <kponire@gmail.com>
2023-09-14 12:50:21 -0700

    ./6-lines | cat -e

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - straight line
+ * Description: draws a straight line in the terminal.
+ * @n: the input line
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+
+	_putchar('\n');
+}

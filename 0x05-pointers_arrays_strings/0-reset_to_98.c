988a70295fcaf85c7dfe351a4afb22ef8d807520
Oluwaponire <kponire@gmail.com>
2023-09-18 03:05:43 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - function to reset an integer
  * Description: updates the value it points to to 98
- * @*n: integer pointer
+ * @n: integer pointer
  */
 
 void reset_to_98(int *n)

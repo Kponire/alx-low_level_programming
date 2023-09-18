bef63cd4544a83e51f237f949127c2eed0c323b2
Oluwaponire <kponire@gmail.com>
2023-09-18 06:33:17 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, in reverse
+ * @s: the input string
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+	int i = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	while (i <= length)
+	{
+		_putchar(s[length]);
+		length--;
+	}
+	_putchar('\n');
+}

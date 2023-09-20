0fdd707aecae16cc8d048549e73b12caec2b1720
Oluwaponire <kponire@gmail.com>
2023-09-20 02:22:51 -0700

    ./0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * strcat - function that concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * Return: appends the src string to the dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}

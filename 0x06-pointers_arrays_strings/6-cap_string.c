7d1cf0d8863a9ae086754ac789c236cb6611dff8
Oluwaponire <kponire@gmail.com>
2023-09-20 11:26:36 -0700

    ./6-cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - function that capitalizes all words of a string.
+ * @s: the string
+ * Return: capitalizes all words
+ */
+
+char *cap_string(char *s)
+{
+	int separator[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int i = 0;
+	int j;
+
+	if (s[i] >= 97 && s[i] <= 122)
+	{
+		s[i] -= 32;
+		i++;
+	}
+
+	while (s[i] != '\0')
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == separator[j])
+			{
+				if (s[i] >= 97 && s[i] <= 122)
+					s[i] -= 32;
+				break;
+			}
+		}
+		i++;
+	}
+}

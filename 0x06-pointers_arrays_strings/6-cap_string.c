405882231e8bcb31082955e4fa6e73979e7b9e1e
Oluwaponire <kponire@gmail.com>
2023-09-20 11:35:32 -0700

    ./6-cap

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -24,8 +24,8 @@ char *cap_string(char *s)
 		{
 			if (s[i] == separator[j])
 			{
-				if (s[i] >= 97 && s[i] <= 122)
-					s[i] -= 32;
+				if (s[i+1] >= 97 && s[i+1] <= 122)
+					s[i+1] -= 32;
 				break;
 			}
 		}

e5336e0e0c2070dbaddbc7f48a7f292c26c528fc
Oluwaponire <kponire@gmail.com>
2023-10-02 04:54:11 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: the number of arguments passed in command line
+ * @argv: the arguments in vector form
+ * Return: 0 indicates success
+ */
+
 int main(int argc, char **argv)
 {
 	int mul = 1;

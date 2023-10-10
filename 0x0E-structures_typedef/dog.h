e71ea2150af4eafb2ccc96977a1a6dcbe81a2195
Oluwaponire <kponire@gmail.com>
2023-10-10 09:56:05 -0700

    new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef dog_h
 #define dog_h
 
+/**
+ * struct dog - new type struct dog
+ * Description: struct dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 #endif

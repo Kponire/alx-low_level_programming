f460159055f7190e3b8ebb08b7c46cdbb0cddda3
Oluwaponire <kponire@gmail.com>
2023-10-10 10:07:59 -0700

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,23 @@
+#include "dog.h"
+
+/**
+ * init_dog -  function that initialize a variable of type struct dog
+ * @d: a pointer to a struct dog
+ * @name: the input name
+ * @age: the input age
+ * @owner: the input owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}

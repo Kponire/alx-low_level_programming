ad5bdea6f91b8c03ac42cb2dc9baf6200fa6959a
Oluwaponire <kponire@gmail.com>
2023-10-10 12:17:22 -0700

    function that frees dogs

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_dog - function that frees dogs
+ * @d: pointer to the structure dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+		free(d);
+	}
+}

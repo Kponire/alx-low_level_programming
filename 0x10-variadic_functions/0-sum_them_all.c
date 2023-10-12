21b87d2c00008ec70b5016c10df9783e23b46516
Oluwaponire <kponire@gmail.com>
2023-10-12 09:12:02 -0700

    void print_all(const char * const format, ...);

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function that returns the sum of all its parameters
+ * @n: the initial parameter
+ * Return: the sum of the va_list
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	unsigned int sum = 0;
+
+	if (n == 0)
+		return (0);
+	va_list list;
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(list, int);
+	}
+	va_end(list);
+	return (sum);
+}

bfe71c6e7b0c497c70eaf50193f380ff23c078ae
Oluwaponire <kponire@gmail.com>
2023-10-12 10:29:10 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 3-main.c 3-print_all.c -o d

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -39,10 +39,11 @@ void print_all(const char * const format, ...)
 				printf("%f", va_arg(list, double)), k = 1;
 				break;
 			case 's':
-				p = va_arg(list, char *), k = 1;
+				str = va_arg(list, char *), k = 1;
 				if (!str)
 				{
-					printf("(nil)"), break;
+					printf("(nil)");
+					break;
 				}
 				printf("%s", str);
 				break;

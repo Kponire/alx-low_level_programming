f3a7e0705c6fa18e483d84697abf35dad5c505ed
Oluwaponire <kponire@gmail.com>
2023-10-12 10:26:09 -0700

    3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - function that prints anything
+ * @format: list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	unsigned int i = 0, j, k = 0;
+	const char arg[] = "cifs";
+	char *str;
+	va_list list;
+
+	va_start(list, format);
+	while (format && format[i])
+	{
+		j = 0;
+		while (arg[j])
+		{
+			if (format[i] == arg[j] && k)
+			{
+				printf(", ");
+				break;
+			} j++;
+		}
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(list, int)), k = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(list, int)), k = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(list, double)), k = 1;
+				break;
+			case 's':
+				p = va_arg(list, char *), k = 1;
+				if (!str)
+				{
+					printf("(nil)"), break;
+				}
+				printf("%s", str);
+				break;
+		} i++;
+	}
+	printf("\n");
+	va_end(list);
+}

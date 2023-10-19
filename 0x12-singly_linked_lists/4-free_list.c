b3cb68ed66ac81c282122161ff4a40caff3bba61
Oluwaponire <kponire@gmail.com>
2023-10-19 07:54:00 -0700

    free_list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,7 +1,5 @@
 #include "lists.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
 
 /**
  * free_list - function that frees a list_t list
@@ -12,7 +10,7 @@ void free_list(list_t *head)
 {
 	if (head == NULL)
 		return;
-	free(head->next);
+	free_list(head->next);
 	free(head->str);
 	free(str);
 }

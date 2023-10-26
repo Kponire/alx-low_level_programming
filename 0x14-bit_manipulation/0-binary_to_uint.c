62cfbfecc6687001647118cc74adad340167efac
Oluwaponire <kponire@gmail.com>
2023-10-26 12:23:00 -0700

    binary

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,24 +8,29 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+	int length = 0, base = 1;
+	unsigned int sum = 0;
 
 	if (b == NULL)
-		return (0);
-
-	while (b[len])
-		len++;
-
-	while (len)
 	{
-		if (b[len - 1] != '0' && b[len -1] != '1')
-			return (0);
-
-		if (b[len - 1] == '1')
-			result +=base;
-		base *= 2;
-		len--;
+		return (0);
 	}
-	return (result);
+	while (b[length] != '\0')
+	{
+		length++;
+	}
+	while (length >= 0)
+	{
+		if (b[length] != '0' && b[length] != '1')
+		{
+			return (0);
+		}
+		if (b[length] == '1')
+		{
+			sum += base;
+		}
+		base *= 2;
+		length--;
+	}
+	return (sum);
 }

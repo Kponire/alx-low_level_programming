2323c877163de92c1dd9f469229337f28c900cb1
kponire <kponire@gmail.com>
2023-12-07 10:57:07 -0800

    0x17

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - function that free a dlistint_t list
+ * @head: pointer to the struct dlistint_t
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *next;
+
+	while (head != NULL)
+	{
+		next = head->next;
+		free(head);
+		head = next;
+	}
+}
